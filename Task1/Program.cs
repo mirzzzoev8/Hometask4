7788732db70f19345c7604ae468fa8a6f62c4fb8
mirzzzoev8 <mirzoevyusufjon8@gmail.com>
2023-12-18 19:44:18 +0500

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿var person = new Person();
+person.FirstName =" FirstName: " + "Yusufjon";
+person.LastName ="LastName: " + "Mirzoev";
+person.Age ="Age: " + 16;
+
+System.Console.WriteLine(person.GetFullInfo()); 
+
+
+
+class Person
+{
+  public string FirstName; 
+  public string LastName;
+  public string Age;
+  
+
+  public string GetFullInfo()
+  {
+    return $"{FirstName} \n {LastName} \n {Age} ";
+  }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿var post = new Post();
+post.Text = " Text = " + "I just updated my post";
+post.Likes = "Likes : " + 10;
+post.Comments = "Comments : " + 5;
+System.Console.WriteLine(post.SetPosts());
+System.Console.WriteLine(post.AddLike());
+System.Console.WriteLine(post.AddComments());
+
+
+class Post
+{
+  public string Text;
+  public string Likes;
+  public string Comments;
+
+  public string SetPosts()
+  {
+    return $"{Text}";
+  }
+  public string AddLike()
+  {
+    return $" {Likes}";
+  }
+  public string AddComments()
+  {
+    return $" {Comments}";
+  }
+  public string GetInfo()
+  {
+   return $"{Text} {Likes} {Comments}";
+  }
+}
